07599d82fa7d4a2f172f31c05d4862b51d27a2ce
cbl <cao_bolun@qq.com>
2026-06-11 17:00:43 +0800

    项目初始化

---
+++ cloudemusic-admin/src/main/java/com/singularity/cloudemusicadmin/CloudemusicAdminApplication.java
@@ -0,0 +1,13 @@
+package com.singularity.cloudemusicadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudemusicAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudemusicAdminApplication.class, args);
+    }
+
+}
