eca938a4461866298c3090c41dd3cf86ef6576b8
cbl <cao_bolun@qq.com>
2026-06-11 17:34:24 +0800

    后端框架搭建完成

---
+++ cloudemusic-admin/src/main/java/com/singularity/cloudemusicadmin/config/RedisConfig.java
@@ -0,0 +1,35 @@
+package com.singularity.cloudemusicadmin.config;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+
+        ObjectMapper om = new ObjectMapper();
+        om.activateDefaultTyping(LaissezFaireSubTypeValidator.instance,
+                ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+
+        Jackson2JsonRedisSerializer<Object> serializer =
+                new Jackson2JsonRedisSerializer<>(om, Object.class);
+
+        template.setKeySerializer(StringRedisSerializer.UTF_8);
+        template.setValueSerializer(serializer);
+        template.setHashKeySerializer(StringRedisSerializer.UTF_8);
+        template.setHashValueSerializer(serializer);
+
+        return template;
+    }
+}
