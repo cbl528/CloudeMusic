87a0c3f8db8a8331905179dc60c46ab30d1131e5
cbl <cao_bolun@qq.com>
2026-06-11 18:55:23 +0800

    启动类添加mapper扫描注释

--- cloudemusic-admin/src/main/java/com/singularity/cloudemusicadmin/CloudemusicAdminApplication.java
+++ cloudemusic-admin/src/main/java/com/singularity/cloudemusicadmin/CloudemusicAdminApplication.java
@@ -1,9 +1,11 @@
 package com.singularity.cloudemusicadmin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.singularity.cloudemusicadmin.mapper")
 public class CloudemusicAdminApplication {
 
     public static void main(String[] args) {
